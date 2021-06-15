2f06eadfd3e884e49471b39803f2a7233336a1ca
Tobin Stultiens <tobin.stultiens@gmail.com>
2021-06-15 23:25:02 +0200

    Finished azure setup script

--- K8s/azure-setup.sh
+++ K8s/azure-setup.sh
@@ -12,12 +12,14 @@ then
 fi
 # Configure Istio
 istioctl install -y
+#istioctl operator init
+#istioctl profile dump default
 kubectl label namespace sharerecipe istio-injection=enabled
 # Setup metallb
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f metallb
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#kubectl apply -f metallb
 # Setup Storage
 kubectl apply -f azure
 # Install rabbitmq
